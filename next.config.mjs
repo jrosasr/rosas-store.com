8582b3637abb6fb67b25b33a2e49fbf5593ae1ad
Juan Rosas <juan.rosasr01@gmail.com>
2024-09-14 18:18:11 -0400

    temp unoptimized images

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,7 @@
 const nextConfig = {
   output: "export",
   images: {
-    domains: ["rosas-store.com"],
-    loader: "akamai",
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384, 512, 768, 1024],
+    unoptimized: true,
   },
 };
 
